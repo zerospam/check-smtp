9af5549508d3705326c73131c9da14ae46eb9677
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2018-10-30 17:19:28 -0400

    feat(SMTP): Adds support for testing Mx Records

--- lib/transport_server.go
+++ lib/transport_server.go
@@ -18,6 +18,19 @@ func (t *TransportServer) Address() string {
 }
 
 func (t *TransportServer) Connect(timeout time.Duration) (conn net.Conn, err error) {
-	conn, err = net.DialTimeout("tcp", t.Address(), timeout)
+	address := t.Address()
+
+	if t.OnMx {
+		mxRecords, errorMx := net.LookupMX(t.Server)
+		if errorMx != nil {
+			return nil, errorMx
+		}
+		if len(mxRecords) == 0 {
+			return nil, fmt.Errorf("MX Records: no mx records found for %s", t.Server)
+		}
+		address = mxRecords[0].Host
+	}
+
+	conn, err = net.DialTimeout("tcp", address, timeout)
 	return conn, err
 }
