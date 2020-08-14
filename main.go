4fb05d5ad58c781bbf13026596321bd8cea8daad
pbb <bourdeau.phil@gmail.com>
2020-08-14 13:48:14 -0400

    Added missing os library

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/zerospam/check-smtp/http/handlers"
 	"github.com/zerospam/check-smtp/lib/environment-vars"
 	"net/http"
+	"os"
 )
 
 func init() {
@@ -13,7 +14,6 @@ func init() {
 }
 
 func main() {
-        
 	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 	err := http.ListenAndServe(fmt.Sprintf(":%s", environmentvars.GetVars().ApplicationPort), nil)
 	if err != nil {
